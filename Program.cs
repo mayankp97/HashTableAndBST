47429b5bcedc5046ac766e2e05f8209ec3ed3d99
Mayank Purohit <mayankpurohit2012@gmail.com>
2020-10-22 23:49:07 +0530

    [Mayank] Initial master branch commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HashTableAndBST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
