bc70e58c0cb54e51db01c7b7d271147b71f15a07
Mayank Purohit <mayankpurohit2012@gmail.com>
2020-10-23 00:25:43 +0530

    [Mayank] Add HashTable implementation and frequency check in a sentence

---
+++ MyMapNode.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTableAndBST
+{
+    public class MyMapNode<K,V>
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            var linkedList = GetLinkedList(position);
+            var item = new KeyValue<K, V>() { Key = key, Value = value };
+            linkedList.AddLast(item);
+        }
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            var linkedList = GetLinkedList(position);
+            foreach (var item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+
+        public void Set(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            var linkedList = GetLinkedList(position);
+            KeyValue<K, V> temp = new KeyValue<K, V>();
+            foreach (var item in linkedList)
+                if (item.Key.Equals(key))
+                {
+                    temp = item;
+                    
+                }
+            temp.Value = value;
+
+        }
+
+        protected int GetArrayPosition(K key)
+        {
+            var position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            var linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+    }
+    public class KeyValue<K, V>
+    {
+        public K Key { get; set; }
+        public V Value { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace HashTableAndBST
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            HashTableDemo();
+        }
+
+        public static void HashTableDemo()
+        {
+            Console.WriteLine("Welcome to Hash Table Demo");
+            var sentence = "To be or not to be";
+            string[] words = sentence.Split(' ');
+            var map = new MyMapNode<string, int>(10);
+            foreach(var word in words)
+            {
+                if (map.Get(word) == 0)
+                    map.Add(word, 1);
+                else
+                {
+                    var freq = map.Get(word) + 1;
+                    map.Set(word, freq);
+                }
+            }
+            Console.WriteLine("Frequency of Word \"be\" is : {0}", map.Get("be"));
         }
     }
 }
